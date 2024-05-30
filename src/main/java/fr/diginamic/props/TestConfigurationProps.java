2c519e1192346ad121d73a71a68aa624e06dffa9
Noémie Letenneur <n.letenneur@gmail.com>
2024-05-30 11:17:39 +0200

    correction fichier config

--- src/main/java/fr/diginamic/props/TestConfigurationProps.java
+++ src/main/java/fr/diginamic/props/TestConfigurationProps.java
@@ -7,6 +7,7 @@ public class TestConfigurationProps {
 	public static void main(String[] args) {
 		ResourceBundle config = ResourceBundle.getBundle("database");
 		String driverName = config.getString("database.driver");
+		System.out.println(driverName);
 
 	}
 
